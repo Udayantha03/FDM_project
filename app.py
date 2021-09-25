ab40d77ba1b12d29f126982711818ae623b2ea5b
Heesha14 <heesha14@gmail.com>
2021-09-25 14:38:22 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, request, jsonify, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def welcome():
+    return render_template('welcome.html')
+
+
+@app.route('/association')
+def association_start():
+    return render_template('association.html')
+
+
+@app.route('/full_list')
+def full_list_start():
+    return render_template('full_list.html')
+
+
+@app.route('/product')
+def product_start():
+    return render_template('product.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)

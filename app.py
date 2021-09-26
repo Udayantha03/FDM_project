ef9efc60b98dacfcc92b42669bf4d9b03daa3724
Ri <szri8802@gmail.com>
2021-09-26 22:45:59 +0530

    classification 21st 09

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
+import numpy as np
 from flask import Flask, request, jsonify, render_template
+import pickle
 
 app = Flask(__name__)
-
+model = pickle.load(open('model.pkl', 'rb'))
 
 @app.route('/')
 def welcome():
@@ -12,10 +14,18 @@ def welcome():
 def association_start():
     return render_template('association.html')
 
-
 @app.route('/classification')
 def classification_start():
     return render_template('classification.html')
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    features = [int(x) for x in request.form.values()]
+    final_features = [np.array(features)]
+    prediction = model.predict(final_features)
+
+    return render_template('classification.html', prediction_text='Customer is {}'.format(prediction))
 
 
 if __name__ == '__main__':
--- model.py
+++ model.py
@@ -2,14 +2,41 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
-
-from imblearn.combine import SMOTETomek
+from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, confusion_matrix, r2_score
 from sklearn.preprocessing import MinMaxScaler
-
+from sklearn.ensemble import RandomForestClassifier
+import pickle
 import warnings
 warnings.filterwarnings('ignore')
 
+
 dataset = pd.read_csv('C:/Users/Raisul Zulfikar/Desktop/Credit Card/Clean_dataset.csv')
+
+dataset = dataset.iloc[:, 1:18]
 print(dataset.head())
+# Your code goes here
+X = dataset.drop('target', axis=1).values # Input features (attributes)
+y = dataset['target'].values # Target vector
+print('X shape: {}'.format(np.shape(X)))
+print('y shape: {}'.format(np.shape(y)))
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
+
+
+rf = RandomForestClassifier(n_estimators=100, criterion='entropy', random_state=0)
+rf.fit(X_train, y_train)
+prediction_test = rf.predict(X=X_test)
+
+# Accuracy on Test
+print("Training Accuracy is: ", rf.score(X_train, y_train))
+# Accuracy on Train
+print("Testing Accuracy is: ", rf.score(X_test, y_test))
+
+# print(rf.predict([[1, 1, 1, 0, 112500, 0, 0, 0, 0, 58.83, 3.106, 16, 2, 1, 1, 1]]))
+
+pickle.dump(rf, open('model.pkl', 'wb'))
+
+model = pickle.load(open('model.pkl', 'rb'))
+print(model.predict([[1, 1, 1, 0, 112500, 0, 0, 0, 0, 58.83, 3.106, 16, 2, 1, 1, 1]]))
 
